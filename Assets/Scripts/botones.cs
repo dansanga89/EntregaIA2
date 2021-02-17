6d6cd015ed8ab69b1cf79a6902cc949033739ca1
Daniel <danielsanchezgamo@gmail.com>
2021-02-17 16:51:49 +0100

    Commit inicial
    Commit inicial

---
+++ Assets/Scripts/botones.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PulsarPiedra() {
+        //Si booleando jugando = true, se hace lo siguiente
+        //Añadir piedra a variable pulsada
+        //Ejecutar IA y dejar true el booleando jugando
+        //Prohibir pulsar hasta que la ia no haya terminado
+    }
+    public void PulsarPapel() {
+    }
+    public void PulsarTijera() { 
+    }
+    public void PulsarLagarto() {
+    }
+    public void PulsarSpock() {
+    }
+}
