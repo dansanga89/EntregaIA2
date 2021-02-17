0daa117f1d4652fd237dc2c628156b6ad8fcbc20
Daniel <danielsanchezgamo@gmail.com>
2021-02-17 18:42:02 +0100

    Con imagenes

--- Assets/Scripts/botones.cs
+++ Assets/Scripts/botones.cs
@@ -7,10 +7,13 @@ public class botones : MonoBehaviour
 {
     [SerializeField]
     private GameObject ImagenIA;
-
+    private bool Jugando;
+    private int Option;
     // Start is called before the first frame update
     void Start()
     {
+        Jugando = false;
+
     }
 
     // Update is called once per frame
@@ -23,24 +26,24 @@ public class botones : MonoBehaviour
         //Si booleando jugando = true, se hace lo siguiente
         //Añadir piedra a variable pulsada
         //Ejecutar IA y dejar true el booleano jugando
-
+        Option = 0;
         ImagenIA.GetComponent<Image>().color = new Color32(54, 32, 75, 100);
 
     }
     public void PulsarPapel() {
-
+        Option = 1;
         ImagenIA.GetComponent<Image>().color = new Color32(2, 42, 78, 100);
     }
     public void PulsarTijera() {
-
+        Option = 2;
         ImagenIA.GetComponent<Image>().color = new Color32(99, 36, 4, 100);
     }
     public void PulsarLagarto() {
-
+        Option = 3;
         ImagenIA.GetComponent<Image>().color = new Color32(124, 167, 3, 100);
     }
     public void PulsarSpock() {
-
+        Option = 4;
         ImagenIA.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
     }
 }
