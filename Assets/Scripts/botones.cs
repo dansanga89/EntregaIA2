58bcfb81ead4383085ac7ef3890f40ba84514211
Daniel <danielsanchezgamo@gmail.com>
2021-02-17 17:40:18 +0100

    Botones Funcionando
    Clares Capuyo

--- Assets/Scripts/botones.cs
+++ Assets/Scripts/botones.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class botones : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject ImagenIA;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,15 +22,25 @@ public class botones : MonoBehaviour
     public void PulsarPiedra() {
         //Si booleando jugando = true, se hace lo siguiente
         //Añadir piedra a variable pulsada
-        //Ejecutar IA y dejar true el booleando jugando
-        //Prohibir pulsar hasta que la ia no haya terminado
+        //Ejecutar IA y dejar true el booleano jugando
+
+        ImagenIA.GetComponent<Image>().color = new Color32(54, 32, 75, 100);
+
     }
     public void PulsarPapel() {
+
+        ImagenIA.GetComponent<Image>().color = new Color32(2, 42, 78, 100);
     }
-    public void PulsarTijera() { 
+    public void PulsarTijera() {
+
+        ImagenIA.GetComponent<Image>().color = new Color32(99, 36, 4, 100);
     }
     public void PulsarLagarto() {
+
+        ImagenIA.GetComponent<Image>().color = new Color32(124, 167, 3, 100);
     }
     public void PulsarSpock() {
+
+        ImagenIA.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
     }
 }
